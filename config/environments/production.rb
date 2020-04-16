a61df13ae6e62ff18a019a06c1fe3b0cf3db30a3
tyatoshi <>
2020-04-16 23:36:30 +0000

    add sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+  :user_name => "zaikoapp@gmail.com",,
+  :password => "vjsnxclxoscmlpjn",
+  :domain => 'herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+  }
+  
 end
