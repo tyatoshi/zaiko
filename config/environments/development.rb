a3fb0196b20f07ca6e26a007d3d99d329f23dc1d
tyatoshi <>
2020-04-16 08:28:37 +0000

    email modfy

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,8 +65,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
-    :user_name => "oki840155@gmail.com",
-    :password => "cbegygnryidbcdhs",
+    :user_name => "zaikoapp@gmail.com",
+    :password => "vjsnxclxoscmlpjn",
     :authentication => :plain,
     :enable_starttls_auto => true
   }
