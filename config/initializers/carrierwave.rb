d0605faeea1a431799ef0ebfadd82eaebc87a65b
tyatoshi <>
2020-04-23 02:50:53 +0000

    aws key

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,17 @@
+unless Rails.env.development? || Rails.env.test?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      # Amazon S3用の設定
+      :provider              => 'AWS',
+      :region                => ENV['S3_REGION'], 
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+    }
+
+    config.fog_directory     =  ENV['S3_BUCKET']
+    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } 
+  end
+  CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/ 
+end
+
+ENV[hoge]
