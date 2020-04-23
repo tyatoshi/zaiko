306541288a0a27cb3fe2fdde7222f59ab8b2305f
tyatoshi <>
2020-04-23 04:23:59 +0000

    sa

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ unless Rails.env.development? || Rails.env.test?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: 'AKIA5ZTIG7KMLR2U4IEO',
-      aws_secret_access_key: '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',
+      aws_access_key_id: 'AKIA5ZTIG7KMIZTL3CFO',
+      aws_secret_access_key: '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',
       region: 'ap-northeast-1'
     }
 
@@ -11,5 +11,3 @@ unless Rails.env.development? || Rails.env.test?
     config.cache_storage = :fog
   end
 end
-
-ENV[hoge]
