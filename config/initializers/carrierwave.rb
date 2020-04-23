f2fcdaa5cb297ca7f2fc6bb069d6995e16d69cdc
tyatoshi <>
2020-04-23 04:15:25 +0000

    sa

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,13 @@
 unless Rails.env.development? || Rails.env.test?
   CarrierWave.configure do |config|
     config.fog_credentials = {
-      # Amazon S3用の設定
-      :provider              => 'AWS',
-      :region                => "ap-northeast-1", 
-      :aws_access_key_id     => "AKIA5ZTIG7KMLR2U4IEO",
-      :aws_secret_access_key => "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"
+      provider: 'AWS',
+      aws_access_key_id: 'AKIA5ZTIG7KMLR2U4IEO',
+      aws_secret_access_key: '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',
+      region: 'ap-northeast-1'
     }
 
-    config.fog_directory     = "zaiko-app"
+    config.fog_directory  = 'zaiko-app'
     config.cache_storage = :fog
   end
 end
